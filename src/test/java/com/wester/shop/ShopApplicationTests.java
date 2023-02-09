df3056490d257eda960aeb7a0c0850007fe4ecc7
dflk <904885508@qq.com>
2023-02-09 15:32:04 +0800

    使用initializr初始化项目

---
+++ src/test/java/com/wester/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wester.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
