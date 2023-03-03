6027326609e7fe0e5d8a559a851d5ee298508597
dflk <904885508@qq.com>
2023-03-03 21:52:03 +0800

    使用flyway管理数据库；使用mybaits-generator生成代码

---
+++ shop-order/src/main/java/com/wester/order/OrderApplication.java
@@ -0,0 +1,14 @@
+package com.wester.order;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(scanBasePackages = "com.wester.api", exclude = DataSourceAutoConfiguration.class)
+@EnableDubbo(scanBasePackages = "com.wester.api")
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
